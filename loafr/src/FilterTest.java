e82c90f220ec5c7af1e0708f04032802f2bef898
Debarati Das <71130128+das00015@users.noreply.github.com>
2022-04-10 22:11:57 -0500

    Add files via upload

--- loafr/src/FilterTest.java
+++ loafr/src/FilterTest.java
@@ -2,69 +2,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * 
- */
+class FilterTest extends Filter  {
 
-/**
- * @author Sean
- *
- */
-class FilterTest extends Filter{
 
-	public FilterTest(String opColumn, int filterValue, boolean filterIncludes) {
-		super(opColumn, filterValue, filterIncludes);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * Test method for {@link Filter#Filter(java.lang.String, int, boolean)}.
-	 */
 	@Test
-	void testFilter() {
-		System.out.println("testing Filter creation");
-		String thisClass = ""+this.getClass();
-		//System.out.println(aClass);
-		assert thisClass.equals("class Filter"): "failed to correctly construct Log Entry";
+	void testFilterValue() {
+		System.out.println("testing Filter value");
+		
+		assert this.opColumn.equals("sugarlevel");
+	
 		System.out.println("test passed");
 	}
 
-	/**
-	 * Test method for {@link Filter#getFilterValue()}.
-	 */
 	@Test
-	void testGetFilterValue() {
-		System.out.println("testing get Filter value function");
-		assert this.getFilterValue()==11 : "Filter value was not set correctly";
+	void testMultipleFilters() {
+		System.out.println("testing Multiple Filters");
+		
+		assert this.bySugarLevel.equals(True);
+
 		System.out.println("test passed");
 	}
 
-	/**
-	 * Test method for {@link Filter#getFilterIncludes()}.
-	 */
 	@Test
 	void testGetFilterIncludes() {
-		System.out.println("testing get Filter includes function");
-		assert this.getFilterIncludes()==false : "Filter includes value was not set correctly";
+		System.out.println("testing testGetFilterIncludes");
+		assert this.getFilterIncludes()==False:"GetFilterIncludes function returned incorrect result";
 		System.out.println("test passed");
 	}
 
-	/**
-	 * Test method for {@link Filter#getBySugarLevel()}.
-	 */
 	@Test
 	void testGetBySugarLevel() {
-		System.out.println("testing get Filter by sugar value function");
-		assert this.getBySugarLevel()==false : "Filter by sugar value was not set correctly";
+		System.out.println("testing GetBySugarLevel()");
+		assert this.getBySugarLevel()== 0:"GetBySugarLevel()function returned incorrect result";
 		System.out.println("test passed");
 	}
+
 	
-	
-	public void runtTests() {
-		this.testFilter();
-		this.testGetFilterValue();
-		this.getFilterIncludes();
-		this.getBySugarLevel();
+	public void runTests() {
+		this.testFilterValue();
+		this.testMultipleFilters();
+		this.testGetFilterIncludes();
+		this.testGetBySugarLevel();
 		
 	}
 
